dc3a46434321843d196df931f4b975b62a6b9d9a
subham-chaudhary <shubhu.uwu@gmail.com>
2024-06-16 17:15:50 +0530

    added: springboot application

---
+++ back-end/src/main/java/com/ipark/adminpanel/IparkAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ipark.adminpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IparkAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IparkAdminApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/ipark/adminpanel/IparkAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipark.adminpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IparkAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
