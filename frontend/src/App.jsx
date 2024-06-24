892388dbf7dd1b0be524049b2f7f1d67646175e1
SOUMYA RANJAN PANDA <116835983+CoderSoumya007@users.noreply.github.com>
2024-06-24 11:39:54 +0530

    LoginPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,127 +1,23 @@
-import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LoginSignupForm from './components/LoginSignupForm'
 import './App.css'
 
-
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginSignupForm />
+  },
+  {
+    path: '/HomePage',
+    element: <HomePage />
+  }
+]);
 
 function App() {
-  const [isUpdateSectionVisible, setIsUpdateSectionVisible] = useState(true);
-  const [objectSectionVisible, setObjectSectionVisible] = useState(true);
-
-  const [updateTogglerRotated, setUpdateTogglerRotated] = useState(false);
-  const [objectTogglerRotated, setObjectTogglerRotated] = useState(false);
-
-  const handleToggle = () => {
-    setIsUpdateSectionVisible(!isUpdateSectionVisible);
-    setUpdateTogglerRotated(!updateTogglerRotated);
-  };
-
   return (
     <>
-      <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">i-Park</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup" >
-              <div class="navbar-nav" style={{ flex: 1, justifyContent: 'space-around' }}>
-                {/* <a class="nav-link active" aria-current="page" href="#">Home</a> */}
-                <a class="nav-link active" href="#">Map</a>
-                <a class="nav-link" href="#">Stats</a>
-                <a class="nav-link" href="#">Record</a>
-                <a class="nav-link" href="#">Users</a>
-                <a class="nav-link" href="#">Others</a>
-                <a class="nav-link" href="#">Control</a>
-                <a class="nav-link" href="#">Settings</a>
-              </div>
-            </div>
-          </div>
-        </nav>
-
-        <div class="container-fluid toggler-container">
-          <button className="btn btn-primary update-toggler" onClick={handleToggle}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className={updateTogglerRotated ? 'rotated' : ''}>
-              <g>
-                <path d="M12,2A10,10,0,1,0,22,12,10.011,10.011,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,12,20Z" />
-                <polygon points="13.293 7.293 8.586 12 13.293 16.707 14.707 15.293 11.414 12 14.707 8.707 13.293 7.293" />
-              </g>
-            </svg>
-
-          </button>
-
-          <button
-            className="btn btn-primary object-toggler"
-            onClick={() => {
-              // toggle object section visibility
-              setObjectSectionVisible(!objectSectionVisible);
-              setObjectTogglerRotated(!objectTogglerRotated);
-            }}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" className={objectTogglerRotated ? 'rotated' : ''}
-            >
-              <path d="M10,20A10,10,0,1,0,0,10,10,10,0,0,0,10,20ZM8.711,4.3l5.7,5.766L8.7,15.711,7.3,14.289l4.289-4.242L7.289,5.7Z" />
-            </svg>
-          </button>
-        </div>
-
-        <div class="container-fluid dashboard-container">
-          <div class="container update-section"
-            style={{ display: isUpdateSectionVisible ? 'block' : 'none' }}>
-            <h2></h2>
-            <div class="card w-100">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">View</a>
-              </div>
-            </div>
-          </div>
-          <div class="container map-section">
-            <h2></h2>
-            <div class="card w-100">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              </div>
-            </div>
-          </div>
-          <div class="container object-section"
-            style={{ display: objectSectionVisible ? 'block' : 'none' }}>
-            <h2></h2>
-            {objectSectionVisible && (
-              <div>
-                <div className="card w-100 mini-map">
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" className="btn btn-primary">View</a>
-                  </div>
-                </div>
-
-                <div className="container text-center minimap-section">
-                  <div className="row row-cols-2 minimaps">
-                    <div className="col">1</div>
-                    <div className="col">2</div>
-                    <div className="col">3</div>
-                    <div className="col">4</div>
-                    <div className="col">5</div>
-                    <div className="col">6</div>
-                    <div className="col">7</div>
-                    <div className="col">8</div>
-                    <div className="col">9</div>
-                    <div className="col">10</div>
-                    <div className="col">11</div>
-                    <div className="col">12</div>
-                    <div className="col">13</div>
-                    <div className="col">14</div>
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ frontend/src/components/LoginSignupForm.jsx
@@ -0,0 +1,147 @@
+import React, { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from "react-router-dom";
+import './LoginPage.css'
+
+const LoginSignupForm = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [currentSlide, setCurrentSlide] = useState("login");
+
+  const handleSlideChange = (event) => {
+    setCurrentSlide(event.target.id);
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const isValidEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (username.trim() === "" && currentSlide === "signup") {
+      if (!toast.isActive('usernameError')) {
+        toast.error("Username cannot be empty!", { toastId: 'usernameError' });
+      }
+      return;
+    }
+    if (email.trim() === '') {
+      toast.error("Email address cannot be empty!", { toastId: 'emailError' });
+      return;
+    }
+    if (!isValidEmail(email)) {
+      toast.error("Invalid email address!", { toastId: 'emailError' });
+      return;
+    }
+    navigate(`/HomePage`);
+  }
+
+  return (
+    <div className='loginpage-container'>
+      <div className='loginpage-wrapper'>
+        <div className="title-text">
+          <div className='title login'>i-Park</div>
+          <div className='title signup'>Signup Form</div>
+        </div>
+        <div className="form-container">
+        <div class="slide-controls">
+          <input
+            class="lg_sn"
+            type="radio"
+            name="slide"
+            id="login"
+            checked={currentSlide === "login"}
+            onChange={handleSlideChange}
+          />
+          <input
+            class="lg_sn"
+            type="radio"
+            name="slide"
+            id="signup"
+            checked={currentSlide === "signup"}
+            onChange={handleSlideChange}
+          />
+          <label for="login" class="slide login">
+            Login
+          </label>
+          <label for="signup" class="slide signup">
+            SignUp
+          </label>
+          <div class="slider-tab"></div>
+        </div>
+
+          <div className="form-inner">
+            {currentSlide === "login" && (
+              <form action="#" className='login'>
+                <div className='field'>
+                  <input type="text" placeholder="Email Address" required value={email} onChange={handleEmailChange} />
+                </div>
+                <div className="field">
+                  <input type="password" placeholder="Password" required value={password} onChange={handlePasswordChange} />
+                </div>
+                <div className="pass-link">
+                  <a href="#">Forgot password?</a>
+                </div>
+                <div className='field btn'>
+                  <div className="btn-layer"></div>
+                  <input type="submit" value="Login" onClick={handleSubmit} />
+                </div>
+                <div className="signup-link">
+                  Do not have an account?{" "}
+                  <a href="#" onClick={() => setCurrentSlide("signup")}>
+                    Signup now
+                  </a>
+                </div>
+              </form>
+            )}
+            { currentSlide === "signup" && (
+              <form action="#" className="signup">
+                <div className='field'>
+                  <input type="text" placeholder="UserName" required value={username} onChange={handleUsernameChange} />
+                </div>
+                <div className='field'>
+                  <input type="text" placeholder="Email Address" required value={email} onChange={handleEmailChange} />
+                </div>
+                <div className='field'>
+                  <input type="password" placeholder="Password" required value={password} onChange={handlePasswordChange} />
+                </div>
+                <div className='field btn'>
+                  <div className="btn-layer"></div>
+                  <input type="submit" value="Signup" onClick={handleSubmit} />
+                </div>
+              </form>
+            )}
+          </div>
+        </div>
+      </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
+};
+
+export default LoginSignupForm;
