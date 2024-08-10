900a9d720adea61e0ac37fb48a671e70ce62dfb9
Pratik KS <sahoopratik5@gmail.com>
2024-08-10 21:47:10 +0530

    Added: Refreshing access token from refresh token Updated: Login controller to set cookie

--- back-end/src/main/java/com/ipark/adminpanel/controller/PublicController.java
+++ back-end/src/main/java/com/ipark/adminpanel/controller/PublicController.java
@@ -1,14 +1,16 @@
 package com.ipark.adminpanel.controller;
 
 import com.ipark.adminpanel.entity.Operator;
+import com.ipark.adminpanel.repository.OperatorRepo;
 import com.ipark.adminpanel.service.OperatorService;
 import com.ipark.adminpanel.service.UserDetailsServiceImpl;
 import com.ipark.adminpanel.utils.JwtUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,6 +26,9 @@ public class PublicController {
 
     @Autowired
     private OperatorService operatorService;
+
+    @Autowired
+    private OperatorRepo operatorRepo;
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -46,17 +51,58 @@ public class PublicController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Operator user) {
+    public ResponseEntity<?> login(@RequestBody Operator user, HttpServletResponse res) {
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPhoneNumber())
             );
             UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
-            String jwt =jwtUtil.generateToken(userDetails.getUsername());
-            return new ResponseEntity<>(jwt, HttpStatus.OK);
-        }catch(Exception e) {
+            Operator users = operatorRepo.findByUserName(user.getUserName());
+            String accessToken = jwtUtil.generateAccessToken(users.getId().toString(), users.getRoles());
+            String refreshToken = jwtUtil.generateRefreshToken(users.getId().toString(), users.getRoles());
+            ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
+                    .httpOnly(true)
+                    .secure(false)
+                    .path("/")
+                    .build();
+            res.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+            cookie = ResponseCookie.from("accessToken", accessToken)
+                    .httpOnly(true)
+                    .secure(false)
+                    .path("/")
+                    .build();
+            res.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+            return new ResponseEntity<>("User Logged In", HttpStatus.OK);
+        } catch (Exception e) {
             log.error("Exception occurred while createAuthenticationToken", e);
             return new ResponseEntity<>("Incorrect username or password", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<?> refreshToken(HttpServletRequest req, HttpServletResponse res) {
+        try {
+            String refreshToken = null;
+            if (req.getCookies() != null) {
+                for (Cookie cookie : req.getCookies()) {
+                    if (cookie.getName().equals("refreshToken")) {
+                        refreshToken = cookie.getValue();
+                    }
+                }
+            }
+            String accessToken = jwtUtil.refreshAccessToken(refreshToken);
+            if (accessToken != null) {
+                ResponseCookie cookie = ResponseCookie.from("accessToken", accessToken)
+                        .httpOnly(true)
+                        .secure(false)
+                        .path("/")
+                        .build();
+                res.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+            }
+            return new ResponseEntity<>("Refreshed Access Token", HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Exception occurred while refreshingAccessToken", e);
+            return new ResponseEntity<>("Refresh Token Expired", HttpStatus.BAD_REQUEST);
         }
     }
 }
