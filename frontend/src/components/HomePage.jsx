ff7d70b560474f726696b8737812bf59de6a2453
SOUMYA RANJAN PANDA <116835983+CoderSoumya007@users.noreply.github.com>
2024-09-08 21:54:03 +0530

    commit

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -96,6 +96,7 @@ const HomePage = () => {
         }
     };
 
+    
 
     //map section for zooming in and out
     // const [zoomLevel, setZoomLevel] = useState(1); // Initial zoom level 
--- frontend/src/components/LeftSidebarContentMap.jsx
+++ frontend/src/components/LeftSidebarContentMap.jsx
@@ -1,7 +1,9 @@
-import {useEffect, useState } from "react";
-const LeftSidebarContentMap=()=>{
-    //for updating the notification slide bar
+import { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './LeftSidebarContentMap.css';
 
+const LeftSidebarContentMap = () => {
     const [updates, setUpdates] = useState([]);
     const [visibleUpdatesCount, setVisibleUpdatesCount] = useState(5);
 
@@ -18,7 +20,7 @@ const LeftSidebarContentMap=()=>{
                 const updatedUpdates = [newUpdate, ...prevUpdates];
                 return updatedUpdates.length > 25 ? updatedUpdates.slice(0, 25) : updatedUpdates;
             });
-        }, 10000); // Add a new update every 3 seconds
+        }, 3000); // Add a new update every 3 seconds
 
         return () => clearInterval(intervalId);
     }, [updates]);
@@ -27,20 +29,26 @@ const LeftSidebarContentMap=()=>{
         setVisibleUpdatesCount(prevCount => Math.min(prevCount + 5, updates.length));
     };
 
-    return <>
-        <h2>Upcoming Updates</h2>
-                        {updates.slice(0, visibleUpdatesCount).map(update => (
-                            <div key={update.id} className="card w-100 mb-3">
-                                <div className="card-body">
-                                    <h5 className="card-title">{update.title}</h5>
-                                    <p className="card-text">{update.description}</p>
-                                    <p className="card-text"><small className="text-muted">{update.timestamp}</small></p>
-                                </div>
-                            </div>
-                        ))}
-                        {visibleUpdatesCount < updates.length && (
-                            <button className="btn btn-primary" onClick={loadMoreUpdates}>Load More</button>
-                        )}
-    </>
-}
+    return (
+        <>
+            <h2>Upcoming Updates</h2>
+            {updates.slice(0, visibleUpdatesCount).map(update => (
+                <div
+                    key={update.id}
+                    className="card w-100 mb-3"
+                >
+                    <div className="card-body">
+                        <h5 className="card-title">{update.title}</h5>
+                        <p className="card-text">{update.description}</p>
+                        <p className="card-text"><small className="text-muted">{update.timestamp}</small></p>
+                    </div>
+                </div>
+            ))}
+            {visibleUpdatesCount < updates.length && (
+                <button className="btn btn-primary" onClick={loadMoreUpdates}>Load More</button>
+            )}
+        </>
+    );
+};
+
 export default LeftSidebarContentMap;
